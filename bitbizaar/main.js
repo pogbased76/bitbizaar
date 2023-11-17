c04d5c9c9c616f78c2ff0c3ffd62dfd2f34600bb
pogbased76 <superuser9976@proton.me>
2023-11-17 21:20:27 +0000

    installed a few basic things

---
+++ bitbizaar/main.js
@@ -0,0 +1,6 @@
+document.querySelector(#app).innerHTML = `
+<div>
+<h1> Hi from vite!</h1>
+
+</div>
+`
---
+++ bitbizaar/vite.config.js
@@ -0,0 +1,11 @@
+export default defineConfig({
+    root: '.', // Points to the directory where `index.html` is located
+    build: {
+        "devPath": "http://localhost:1420",
+
+      outDir: 'dist',
+      // other options...
+    },
+    // other configurations...
+  });
+  
