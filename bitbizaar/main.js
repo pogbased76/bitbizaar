f37001a65a1b79d85a9e01fccf83d17e596d8c7f
pogbased76 <superuser9976@proton.me>
2023-11-21 02:41:55 +0000

    aaaaa

--- bitbizaar/main.js
+++ bitbizaar/main.js
@@ -1,6 +1,50 @@
-document.querySelector(#app).innerHTML = `
-<div>
-<h1> Hi from vite!</h1>
+// Import necessary libraries
+import IPFS from 'ipfs-core';
+import { create } from '@orbitdb/core';
+import * as OrbitDBIdentityProviderEthereum from '@orbitdb/identity-provider-ethereum';
+import { Identities, addIdentityProvider } from '@orbitdb/core';
 
-</div>
-`
+async function init() {
+    // Initialize IPFS
+    const ipfs = await IPFS.create();
+
+    // Add Ethereum Identity Provider to OrbitDB
+    addIdentityProvider(OrbitDBIdentityProviderEthereum);
+
+    // Create identities using the IPFS instance
+    const identities = await Identities({ ipfs });
+
+    // Check for Ethereum provider (e.g., MetaMask)
+    if (typeof window.ethereum !== 'undefined') {
+        // Get user's Ethereum account and signature
+        const { account, signature } = await getSignature();
+
+        // Create an Ethereum identity with a dynamic user ID
+        const identity = await identities.createIdentity({ id: account, type: 'ethereum' });
+
+        // Display user information
+        displayUserInfo(account, signature);
+    } else {
+        // Handle the absence of an Ethereum provider
+        document.querySelector('#app').innerHTML = '<p>Ethereum wallet not detected. Please install MetaMask.</p>';
+    }
+}
+
+async function getSignature() {
+    const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
+    const account = accounts[0];
+    const signature = await window.ethereum.request({
+        method: 'personal_sign',
+        params: [account, 'Please sign this message to confirm your identity.'],
+    });
+    return { account, signature };
+}
+
+function displayUserInfo(account, signature) {
+    document.querySelector('#userInfo').innerHTML = `
+        <p>Address: ${account}</p>
+        <p>Signature: ${signature}</p>`;
+}
+
+// Initialize everything on window load
+window.addEventListener('load', init);
--- bitbizaar/vite.config.js
+++ bitbizaar/vite.config.js
@@ -1,11 +1,30 @@
-export default defineConfig({
-    root: '.', // Points to the directory where `index.html` is located
-    build: {
-        "devPath": "http://localhost:1420",
+import { defineConfig } from 'vite';
+import wasm from 'vite-plugin-wasm';
+import path from 'path'; // Import path for resolving aliases
+import nodePolyfills from 'rollup-plugin-node-polyfills';
+import commonjs from '@rollup/plugin-commonjs';
 
-      outDir: 'dist',
-      // other options...
+export default defineConfig({
+  plugins: [wasm(),   nodePolyfills(), commonjs()],
+  root: '.', // Points to the directory where `index.html` is located
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      // Ignore specific Node.js modules
+      external: ['os', 'net', 'dgram', 'child_process', 'fs', 'http', 'https', 'path', 'crypto', 'stream', 'assert', 'util', 'module', 'timers', 'constant','SIGNALS'],
     },
-    // other configurations...
-  });
+    // other options...
+  },
+  server: {
+    host: true, // Listen on all network interfaces
+    port: 1420, // Specify the port
+    // other server options...
+  },
+  resolve: {
+    alias: {
   
+    },
+  },
+});
+//  // Alias 'node:os' to the shim file
+      // ... other aliases
